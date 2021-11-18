3697cb377accb3202182079beaf79062d6f4ad24
wodsonluiz <wodsonluiz@live.com>
2021-11-17 22:37:26 -0300

    Ajusting configs

--- src/UI/ProjectCleanArch.Api/Startup.cs
+++ src/UI/ProjectCleanArch.Api/Startup.cs
@@ -1,14 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ProjectCleanArch.Ioc;
 
 namespace ProjectCleanArch.Api
 {
@@ -24,8 +19,8 @@ namespace ProjectCleanArch.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddInfrasTructure(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
