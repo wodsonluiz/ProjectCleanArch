a6d519ca4d62c65082df9dfc29c493392cd17212
wodsonluiz <wodsonluiz@live.com>
2021-11-23 22:56:11 -0300

    Add project Unit test

---
+++ src/Application/ProjectCleanArch.Application.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ProjectCleanArch.Application.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
