6bb56493f41b59d860eeab667cb8a73b8e5cb213
wodsonluiz <wodsonluiz@live.com>
2022-03-01 14:31:10 -0300

    Ajusting log

--- src/Infrastructure/ProjectCleanArch.Ioc/DependencyInjectionLogger.cs
+++ src/Infrastructure/ProjectCleanArch.Ioc/DependencyInjectionLogger.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
+using Serilog.Events;
 
 namespace ProjectCleanArch.Ioc
 {
@@ -9,6 +10,9 @@ namespace ProjectCleanArch.Ioc
         public static void CreateLoggingSingleton(this IServiceCollection services, IConfiguration configuration)
         {
             Log.Logger = new LoggerConfiguration()
+                 .WriteTo.Console()
+                 .WriteTo.File(configuration.GetSection("LogFile").GetSection("Path").Value,
+                               restrictedToMinimumLevel: GetLevelLogg(configuration.GetSection("LogFile").GetSection("Path").Value))
                  .Enrich.WithProperty("Project", configuration.GetSection("LogSeq").GetSection("Project").Value)
                  .Enrich.WithProperty("Environment", configuration.GetSection("LogSeq").GetSection("Env").Value)
                  .WriteTo.Seq(configuration.GetSection("LogSeq").GetSection("Host").Value)
@@ -16,5 +20,12 @@ namespace ProjectCleanArch.Ioc
 
             services.AddSingleton(Log.Logger);
         }
+
+        private static LogEventLevel GetLevelLogg(string configuration) => configuration switch
+        {
+            "Debug" => LogEventLevel.Debug,
+            "Information" => LogEventLevel.Information,
+            _ => LogEventLevel.Verbose,
+        };
     }
 }
