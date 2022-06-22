5823348dee49b60de9deb79c93304139d44901ed
wodsonluiz <wodsonluiz@live.com>
2022-06-21 22:47:39 -0300

    start implementation circuit breack

---
+++ src/UI/Client.Api/Resilience/CircuitBreaker.cs
@@ -0,0 +1,14 @@
+using Polly;
+using Polly.CircuitBreaker;
+
+namespace Client.Api.Resilience
+{
+    public static class CircuitBreaker
+    {
+        public static AsyncCircuitBreakerPolicy CreatePolicy()
+        {
+            return Polly.
+                
+        }
+    }
+}
